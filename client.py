9cf35c907f0f8947b926fdb17a369e66021255ca
apezkin <ape.zkin@gmail.com>
2021-04-04 18:39:34 +0300

    Git initial commit
    Server, client and db
    Client can send messages to a topic,
    get messages from a topic
    and search wikipedia

---
+++ client.py
@@ -0,0 +1,49 @@
+import xmlrpc.client
+import datetime
+
+def main():
+    s = xmlrpc.client.ServerProxy('http://localhost:8000', allow_none=True)
+    userInput = 0
+    
+    while True:
+        print("1) Send msg")
+        print("2) Get topic msgs")
+        print("3) Search wikipedia")
+        print("0) Quit")
+        try:
+            userInput = int(input("Choice: "))
+
+            if userInput == 0:
+                break
+
+            elif userInput == 1:
+                msgTopic = input("Give msg topic: ")
+                msgHeader = input("Give msg header: ")
+                msgText = input("Give msg text: ")
+                msgTime = datetime.datetime.now().strftime("%c")
+                print("\nMessage topic:", msgTopic, "\nMessage text:", msgText, "\nMessage time:", msgTime, "\n")
+                res = s.sendMsg(msgTopic, msgHeader, msgText, msgTime)
+                print(res)
+
+            elif userInput == 2:
+                topic = input("Give topic to fetch: ")
+                res = s.getMsg(topic)
+                for i in res:
+                    print(i)
+
+            elif userInput == 3:
+                topic = input("Give topic: ")
+                searchTerms = input("Give search terms to search with: ")
+                time = datetime.datetime.now().strftime("%c")
+                res = s.searchWiki(topic, searchTerms, time)
+                print(res)
+
+            else:
+                print("Unknown input")
+        except Exception as e:
+            print("Error:", e)
+
+        
+
+
+main()
